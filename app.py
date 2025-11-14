523af7ce31757502d18fafec4fed19c1a5f0a75b
Angeli Florez <angelixflorez@gmail.com>
2025-11-13 19:43:53 -0500

    Remove debug print and local server start code

--- app.py
+++ app.py
@@ -44,8 +44,3 @@ def handle_qr_scan():
                            datos=datos_mermelada,
                            nombre_mascota=nombre_archivo,
                            mensaje_sostenible=mensaje_sostenible)
-
-# Solo para probar en tu computadora:
-if __name__ == '__main__':
-    print("Iniciando servidor local en http://127.0.0.1:5000/qr-scan/")
-    app.run(debug=True)
